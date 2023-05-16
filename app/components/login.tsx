ac9c9a1fd0632ea076ef9362b65f9eda3202905c
黎志钧 <lizjun@rpgmoba.com>
2023-05-16 10:42:09 +0800

    * cancel fromlogin

--- app/components/login.tsx
+++ app/components/login.tsx
@@ -1,8 +1,5 @@
 import * as React from "react";
-import * as ReactDOM from "react-dom";
-import chatStyle from "./chat.module.scss";
 import loginStyle from "./login.module.scss";
-import LoginForm from "./LoginForm";
 export function Login() {
   return (
     <div className={loginStyle["login-page"]}>
