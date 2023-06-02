5fd2f09d59e32689f2ecf4631e6dd3084f9dcec5
黎志钧 <lizjun@rpgmoba.com>
2023-06-02 11:04:25 +0800

    * update apiHost

--- app/config/server.ts
+++ app/config/server.ts
@@ -40,6 +40,6 @@ export const getServerSideConfig = () => {
     proxyUrl: process.env.PROXY_URL,
     isVercel: !!process.env.VERCEL,
     hideUserApiKey: !!process.env.HIDE_USER_API_KEY,
-    apiHost: "http://gpt-api.logues.cn",
+    apiHost: "https://gpt-api.logues.cn",
   };
 };
