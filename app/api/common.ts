d835e12fefac54f28171e16549ec3ed039147040
黎志钧 <lizjun@rpgmoba.com>
2023-06-13 19:47:25 +0800

    * update api ip

--- app/api/common.ts
+++ app/api/common.ts
@@ -1,7 +1,7 @@
 import { NextRequest } from "next/server";
 
 const OPENAI_URL = "api.openai.com";
-const DEFAULT_PROTOCOL = "https";
+const DEFAULT_PROTOCOL = "http";
 const PROTOCOL = process.env.PROTOCOL ?? DEFAULT_PROTOCOL;
 const BASE_URL = process.env.BASE_URL ?? OPENAI_URL;
 
