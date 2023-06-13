d9e9825daab6520d41678460250f58b1ac10befc
黎志钧 <lizjun@rpgmoba.com>
2023-06-13 20:42:50 +0800

    * update img ip

--- app/components/recharge.tsx
+++ app/components/recharge.tsx
@@ -113,14 +113,14 @@ export function Recharge() {
               <div>
                 <img
                   style={{ width: "300px" }}
-                  src="https://gpt-api.limq.cn/img/wx_user.jpg"
+                  src="https://106.55.158.200/img/wx_user.jpg"
                   alt=""
                 />
               </div>
               <div>
                 <img
                   style={{ width: "300px" }}
-                  src="https://gpt-api.limq.cn/img/wx_pay.jpg"
+                  src="https://106.55.158.200/img/wx_pay.jpg"
                   alt=""
                 />
               </div>
